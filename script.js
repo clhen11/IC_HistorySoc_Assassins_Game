b8a9b68fe5dcaae684cc7eb0fa04c8c08e320fb8
Cazza <clhen11@gmail.com>
2025-09-08 21:56:54 +0100

    Basic Homepage layout finished

---
+++ script.js
@@ -0,0 +1,15 @@
+// Select all scroll sections
+// const scrolls = document.querySelectorAll('.scroll');
+
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('visible');
+//     }
+//   });
+// }, { threshold: 0.2 }); // triggers when 20% of the scroll is visible
+
+// scrolls.forEach(scroll => {
+//   observer.observe(scroll);
+// });
+
